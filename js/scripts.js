65f7f2eb9fa28a6448db0fcdd213135238eabc47
Beth Fostergit config --global user.namegit config --global user.name Beth <beth@techgir1.com>
2016-01-18 12:14:58 -0800

    added more functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
+$(document).ready(function() {
+	$("form#leap").submit(function(event) {
+	   var year = parseInt($("input#year").val());
+	  
 var leapYear = function(year) {
 	return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0); 
+
 };
+
+$("#result").empty().append(year);
+$("#answer").show();
+
+  });
+});
  
