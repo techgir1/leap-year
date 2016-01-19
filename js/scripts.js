5fd267321f9df44594aa79ecc820d5a879e21745
Beth Fostergit config --global user.namegit config --global user.name Beth <beth@techgir1.com>
2016-01-18 22:48:55 -0800

    working model

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
-$(document).ready(function() {
-	$("form#leap").submit(function(event) {
-	   var year = parseInt($("input#year").val());
-	   var result = leapYear(year);
-	  
 var leapYear = function(year) {
-	if ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0)) { 
+	if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) { 
 		return true;
 	  } else {
 	  	return false;
 	  }
 };
 
-$("#result").empty().append(year);
+$(document).ready(function() {
+	$("form#leap").submit(function(event) {
+	   var year = parseInt($("input#year").val());
+	   var result = leapYear(year);
+
+	   $(".year").text(year);
+
+	   if(!result) {
+	   	$ (".result2").text("not");
+	   } else {
+	   	$(".result2").text("");
+       }
+	   	$("#answer").show();
+	  
+
+
 $("#answer").show();
 
 	event.preventDefault();
