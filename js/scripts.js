a26da96d775b660de17cc6ac59daad45a7ec7737
Beth Fostergit config --global user.namegit config --global user.name Beth <beth@techgir1.com>
2016-01-18 22:20:50 -0800

    added if/else statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
 	$("form#leap").submit(function(event) {
 	   var year = parseInt($("input#year").val());
+	   var result = leapYear(year);
 	  
 var leapYear = function(year) {
-	return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0); 
-
+	if ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0)) { 
+		return true;
+	  } else {
+	  	return false;
+	  }
 };
 
 $("#result").empty().append(year);
@@ -14,4 +18,14 @@ $("#answer").show();
 
   });
 });
+
+
+
+
+
+
+
+
+
+
  
